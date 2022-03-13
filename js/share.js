ba12fab192dbf67c4cef939af595d03a13c11a09
jihee <oosoojh31@gmail.com>
2022-03-13 20:05:35 +0900

    Create share.js

---
+++ js/share.js
@@ -0,0 +1,34 @@
+const url = "https://urperfume.netlify.app/";
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = "향으로 보는 성격유형 결과"
+  const shareDesc = infoList[resultAlt].name;
+  const shareImg = url + 'img/image-' + resultAlt + '.jpg';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDesc,
+      imageUrl: shareImg,
+      link: {
+        mobileWebUrl: shareURL,
+        WebUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          WebUrl: shareURL
+        },
+      },
+    ]
+  });
+
+}
